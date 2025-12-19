192f8792d03af34d601c5e0446b04641c55e1196
hanyj0213 <hanyj0213@gmail.com>
2025-12-19 05:02:52 +0000

    tq

---
+++ Movie recommendation.py
@@ -0,0 +1,27 @@
+from dotenv import load_dotenv
+import os
+import streamlit as st
+import pandas as pd
+from pandasai import PandasAI
+from pandasai.llm.openai import OpenAI
+
+llm = OpenAI(api_token="sk-...OdwA")
+pandas_ai = PandasAI(llm)
+
+st.title("Movie Recommender")
+uploaded_file = st.file_uploader("Upload a CSV file with movie data", type=["csv"])
+
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file)
+    st.write("Uploaded Movie Data:")
+    st.write(df.head(3))
+    st.dataframe(df)
+
+    Prompt = st.text_area("Enter your movie title or preferences or query:")
+
+    if st.button("get Recommendations"):
+        if Prompt:
+            with st.spinner("Generating recommendations..."):
+                st.write(pandas_ai.run(df, Prompt=Prompt))
+    else:
+        st.warning("Please upload a CSV file to get started.")
